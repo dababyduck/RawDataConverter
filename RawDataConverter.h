06dab085d31725b641b48a32e0f45d4cfe57aa24
dababyduck <86234739+dababyduck@users.noreply.github.com>
2025-06-21 19:06:21 +0500

    Update RawDataConverter.h
    Added float version of the functions

--- RawDataConverter.h
+++ RawDataConverter.h
@@ -5,10 +5,11 @@
 /*
 This library converts raw data formed in one string into an easy-to-use array with
 keys and values.
-I originally developed it to convert esp-now's data for my projects
-To change the final array's 
+I originally developed it to convert esp-now's data for my projects.
 
-Version 1.0
+Version 1.1 
+Added float variant of the functions
+
 */
 
 namespace Converter {
@@ -17,12 +18,22 @@ namespace Converter {
         String key;
         int value;
     };
+
+    struct dictf
+    {
+      String key;
+      float value;
+    };
     
     const dict* convert(const char* rawdata);
+    const dictf* convertf(const char* rawdata);
     int getPacketSize();
+    int getPacketSizef();
     void setDataTemplate(dict* newData,int size);
+    void setDataTemplatef(dictf* newData,int size);
     //int getValue(dict* Data,String Key);
     int getValue(String Key);
+    float getValuef(String Key);
 }
 
 
